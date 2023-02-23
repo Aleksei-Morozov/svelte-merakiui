1604cdb2d404c74107f76d9082c9f65d245e4eea
Aleksei Morozov <aleksei.vl.morozov@gmail.com>
2023-02-23 20:11:48 +0500

    Add error to Helper

--- src/routes/helper/+page.svelte
+++ src/routes/helper/+page.svelte
@@ -2,12 +2,18 @@
 	import Helper from '$lib/Helper.svelte';
 	import H1 from '$lib/H1.svelte';
 	import Section from '../Section.svelte';
-
-	const code = `<Helper>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Helper>`;
 </script>
 
 <H1>Helper</H1>
 
-<Section heading="Helper text" {code}>
+<Section
+	heading="Helper text"
+	code={'<Helper>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Helper>'}
+>
 	<Helper>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Helper>
 </Section>
+
+<Section
+	heading="Error message"
+	code={'<Helper error>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Helper>'}
+/>
